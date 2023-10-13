ebf3219a0e92d64aad3fce6695040eb2c8c783e6
Ryan Edwards <101122687+Regularguyy21@users.noreply.github.com>
2023-10-13 11:29:39 -0400

    Delete statblock5e-master directory

--- statblock5e-master/src/js/creature-heading.js
+++
@@ -1,9 +0,0 @@
-import {createCustomElement} from '.statblock5e-master/helpers/create-custom-element.js';
-
-fetch('./statblock5e-master/src/templates/creature-heading.html')
-  .then(stream => stream.text())
-  .then(htmlContent => {
-    let contentNode =
-      document.createRange().createContextualFragment(htmlContent);
-    createCustomElement('creature-heading', contentNode);
-  });
